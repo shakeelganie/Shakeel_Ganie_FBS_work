1548b198ee96c2c5fef4fa27b052858f435e58aa
Shakeel Ganie <shakeelganie43@gmail.com>
2025-06-26 21:36:07 +0530

    Delete Assignment1/Assignment2 directory

--- Assignment1/Assignment2/sp_cp.py
+++
@@ -1,8 +0,0 @@
-# calculate selling price of a book based on cost price and discount
-
-cp=int(input("Enter the cost price of the book: "))
-discount=int(input("Enter the discount percentage: "))
-
-discount_amount=cp*discount/100
-sp=cp-discount_amount
-print("The selling price of the book is: ",sp)
--- Assignment1/Assignment2/temp_conversion.py
+++
@@ -1,4 +0,0 @@
-# temperature converion from celsuis to farenhiet
-celsius= int(input("Enter the temp in celsius: "))
-farenhiet= celsius*(9/5)+32
-print("Temperature from celsius to Farenhiet is: ",round(farenhiet,2))
