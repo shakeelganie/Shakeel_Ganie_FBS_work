cda663bd7fd2ed172baf82c44ca873ab81490a2f
Shakeel Ganie <shakeelganie43@gmail.com>
2025-06-26 21:36:53 +0530

    Delete Assignment1 directory

--- Assignment1/angle_area.py
+++
@@ -1,6 +0,0 @@
-#  find the third angle of a traingle, two are given
-
-angle1=int(input("Enter the angle1 of a traingle: "))
-angle2=int(input("Enter the angle2 of a traingle: "))
-angle3=180-(angle1+angle2)
-print("The third angle of a traingle is: ",angle3)
