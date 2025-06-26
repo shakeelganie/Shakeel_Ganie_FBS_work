8a8763073228cc7485625b37c016fdd0c1bb3ed8
Shakeel Ganie <shakeelganie43@gmail.com>
2025-06-26 22:01:21 +0530

    Assignment 2 completed

---
+++ Assignment2/temp_conversion.py
@@ -0,0 +1,4 @@
+# temperature converion from celsuis to farenhiet
+celsius= int(input("Enter the temp in celsius: "))
+farenhiet= celsius*(9/5)+32
+print("Temperature from celsius to Farenhiet is: ",round(farenhiet,2))
